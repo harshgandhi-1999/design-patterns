46780b338908991431b75b7fb875df5bb592759e
harsh-the-innovator <harshgandhi043@gmail.com>
2025-09-06 20:24:32 +0530

    Implemented Strategy Design Pattern

---
+++ src/main/java/behavioural/StrategyPattern/StrategyPatternDemo.java
@@ -0,0 +1,12 @@
+package behavioural.StrategyPattern;
+
+public class StrategyPatternDemo {
+
+    public static void main(String[] args) {
+        Vehicle sportsVehicle = new SportsVehicle();
+
+        sportsVehicle.drive();
+
+        // also see gfg for sorting algo example
+    }
+}
