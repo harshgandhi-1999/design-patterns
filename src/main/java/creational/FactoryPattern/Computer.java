7e53f0ba1dad05cfde6df2c57a16a09390046a31
harsh-the-innovator <harshgandhi043@gmail.com>
2026-05-18 19:19:35 +0530

    Implemented Factory Method Design Pattern

---
+++ src/main/java/creational/FactoryPattern/Computer.java
@@ -0,0 +1,17 @@
+package creational.FactoryPattern;
+
+public abstract class Computer {
+
+    // Super class in factory design pattern can be an interface, abstract class or a normal java class.
+    // For our factory design pattern example, we have abstract super class with overridden toString() method
+    // for testing purpose.
+    public abstract String getRAM();
+    public abstract String getHDD();
+    public abstract String getCPU();
+
+    @Override
+    public String toString(){
+        return "RAM= "+this.getRAM()+", HDD="+this.getHDD()+", CPU="+this.getCPU();
+    }
+
+}
