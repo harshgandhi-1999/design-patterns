11b9044c56f18cbe5ed74a173cdc27ad5716b99e
harsh-the-innovator <harshgandhi043@gmail.com>
2026-05-19 21:01:35 +0530

    Added some comments for better understanding

--- src/main/java/structural/AdapterPattern/ChargingAdapter.java
+++ src/main/java/structural/AdapterPattern/ChargingAdapter.java
@@ -1,7 +1,11 @@
 package structural.AdapterPattern;
 
+
+// Here Adapter implements the target interface
 public class ChargingAdapter implements IphoneChargingInterface{
 
+    // this is Adaptee - the existing class which is incompatible that needs integration
+    // so we have android charger which is incompatible to iphone charging interface
     private final AndroidChargingInterface androidCharger;
 
     public ChargingAdapter(AndroidChargingInterface androidCharger) {
